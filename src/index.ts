b234d63951a2a83402526b50e7ba9c5a08855c02
kiran vignesh <rajkiranvignesh369@gmail.com>
2025-05-25 20:09:48 +0530

    altered carbon

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,9 @@ app.use(express.json());
 // endpoint
 app.use('/api/generate', generateRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+//npm run dev
